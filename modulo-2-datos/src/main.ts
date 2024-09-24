1b9f95368430116989c7c5443d6393aff74e6fa0
Jose Luis Ascencio - Personal <jlmurgas07@gmail.com>
2024-09-23 23:22:09 -0500

    Finalizado el laboratorio del modulo 2 Tipos de Datos y Operadores

---
+++ modulo-2-datos/src/main.ts
@@ -0,0 +1,5 @@
+import './style.css';
+
+console.log('Total a pagar por persona es: ' + (120 - 18) / 6);
+
+// El ticket es de 120, a este se le restan los 18 que yo voy a pagar para las bebidas y el resultado se divide entre 6 (deberia de ser entre 7 pues son 6 amigos y yo. Pero como yo ya pague las bebidas...)
