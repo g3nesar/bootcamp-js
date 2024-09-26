d02ec32e2d961d20c6700cc79b8c88d777363d81
Jose Luis Ascencio - Personal <jlmurgas07@gmail.com>
2024-09-25 22:30:07 -0500

    En este commit se agrega la funcionalidad de que el usuario pueda mostrar un numero manualmente, olvide agregarlo en el commit anterior

--- modulo-4-funciones/src/main.ts
+++ modulo-4-funciones/src/main.ts
@@ -4,12 +4,14 @@ import './style.css';
 const anteriorBtn = document.querySelector('.anteriorBtn') as HTMLButtonElement;
 const resetBtn = document.querySelector('.resetBtn') as HTMLButtonElement;
 const siguienteBtn = document.querySelector('.siguienteBtn') as HTMLButtonElement;
+const insertarNumero = document.querySelector('.insertarNum') as HTMLButtonElement;
 
 // CAPTURA DE TURNO
 let turno = document.querySelector('h1.numero-turno') as HTMLHeadingElement;
 let nuevoTurno: number = Number(turno.textContent);
 console.log(`Valor antes de hacer click en botones: ${nuevoTurno}`);
 
+// Funcion para aumentar un numero de turno.
 function aumentarTurno(): void {
   if (nuevoTurno < 0) {
     anteriorBtn.disabled = true;
@@ -22,6 +24,7 @@ function aumentarTurno(): void {
   turno.innerHTML = nuevoTurno.toString().padStart(2, '0');
 }
 
+// Funcion para restar un numero de turno.
 function restarTurno(): void {
   if (nuevoTurno <= 0) {
     anteriorBtn.disabled = true;
@@ -34,11 +37,19 @@ function restarTurno(): void {
   turno.innerHTML = nuevoTurno.toString().padStart(2, '0');
 }
 
+// Funcion para resetear numero de turno.
 function resetearTurno(): void {
   nuevoTurno = 0;
   turno.innerHTML = '0';
 }
 
+// Funcion para insertar numero personalizado
+function numeroPersonalizado(): void {
+  nuevoTurno = Number((document.querySelector('.numeroUsuario') as HTMLInputElement).value);
+  turno.innerHTML = nuevoTurno.toString().padStart(2, '0');
+}
+
 siguienteBtn.addEventListener('click', aumentarTurno);
 anteriorBtn.addEventListener('click', restarTurno);
 resetBtn.addEventListener('click', resetearTurno);
+insertarNumero.addEventListener('click', numeroPersonalizado);
