0bb2a0a7618d9bd3d9a0db986d2b972af0ea8416
Jose Luis Ascencio - Personal <jlmurgas07@gmail.com>
2024-11-22 01:50:15 -0600

    Finalizado el laboratorio del modulo 5 condicionales

--- modulo-5-condicionales/src/main.ts
+++ modulo-5-condicionales/src/main.ts
@@ -1,35 +1,50 @@
 import './style.css';
 
 // Variables
-const puntuacion = document.querySelector('.puntuacion') as HTMLDivElement | null;
-const pedirCarta = document.querySelector('.pedirCarta') as HTMLButtonElement | null;
+const puntuacionActual = document.querySelector('.puntuacion') as HTMLDivElement | null;
+const botonPedirCarta = document.querySelector('.pedirCarta') as HTMLButtonElement | null;
+const botonMePlanto = document.querySelector('.mePlanto') as HTMLButtonElement | null;
+const botonSimular = document.querySelector('.simular') as HTMLButtonElement | null;
+const botonNuevaPartida = document.querySelector('.nuevaPartida') as HTMLButtonElement | null;
 const imagenCarta = document.querySelector('.carta') as HTMLImageElement | null;
 
 
-if (!puntuacion || !pedirCarta || !imagenCarta) {
+if (!puntuacionActual || !botonPedirCarta || !botonMePlanto || !botonSimular || !botonNuevaPartida || !imagenCarta) {
   console.error('Error! No se encontraron todos los elementos del DOM');
   alert('Hay un error en el sistema, por favor contactar con el administrador');
 } else {
-  let puntuacionUsuario: number = 0;
-  const muestraPuntuacion = () => {
+  puntuacionActual.innerHTML = "0";
 
-  };
-
-  const dameCarta = (): number => {
-    const generarNuevaCarta = (): number => Math.floor(Math.random() * 10);
+  // Funcion para generar una carta nueva
+  const dameCarta = (): void => {
+    const generarNuevaCarta = (): number => Math.floor(Math.random() * 10) + 1;
     let nuevaCarta = generarNuevaCarta();
     if (nuevaCarta > 7) {
       nuevaCarta = nuevaCarta + 2;
-      return nuevaCarta;
+      muestraCarta(nuevaCarta);
+      muestraPuntuacion(nuevaCarta);
     } else {
-      return nuevaCarta;
+      muestraCarta(nuevaCarta);
+      muestraPuntuacion(nuevaCarta);
     }
   };
 
+  // Funcion para mostrar la puntuacion en el frontend
+  const muestraPuntuacion = (cartaPedida: number): void => {
+    if (cartaPedida > 7) {
+      cartaPedida = 0.5;
+    }
+    let puntuacionUsuario = parseFloat(puntuacionActual.innerHTML) + cartaPedida;
+    puntuacionActual.innerHTML = String(puntuacionUsuario);
+    if (puntuacionUsuario > 7.5) {
+      botonPedirCarta.disabled = true;
+      alert("Lo siento, tu puntaje es mayor al objetivo. Comienza una nueva partida.");
+    }
+  };
 
+  // Funcion para mostrar la imagen de la carta
   const muestraCarta = (carta: number): void => {
-    let cartaPedida: number = dameCarta();
-    switch (cartaPedida) {
+    switch (carta) {
       case 1:
         imagenCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/1_as-copas.jpg";
         break;
@@ -46,7 +61,7 @@ if (!puntuacion || !pedirCarta || !imagenCarta) {
         imagenCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/5_cinco-copas.jpg";
         break;
       case 6:
-        imagenCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/7_siete-copas.jpg";
+        imagenCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/6_seis-copas.jpg";
         break;
       case 7:
         imagenCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/7_siete-copas.jpg";
@@ -63,26 +78,56 @@ if (!puntuacion || !pedirCarta || !imagenCarta) {
       default:
         imagenCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/back.jpg";
     }
-
   };
+
+  //Evaluar Puntuacion
+
+  const evaluarPuntuacion = () => {
+    let puntos = parseFloat(puntuacionActual.innerHTML);
+    if (puntos === 7.5) {
+      alert("¡ Lo has clavado! ¡Enhorabuena!");
+    } else if (puntos <= 4) {
+      alert("Has sido muy conservador");
+    } else if (puntos === 5) {
+      alert("Te ha entrado el canguelo eh?");
+    } else if (puntos >= 6 || puntos <= 7) {
+      alert("Casi casi...");
+    }
+  }
+
+  botonPedirCarta.addEventListener('click', () => {
+    dameCarta();
+  });
+
+  botonMePlanto.addEventListener('click', () => {
+    botonPedirCarta.disabled = true;
+    evaluarPuntuacion();
+  });
+
+  botonNuevaPartida.addEventListener('click', () => {
+    imagenCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/back.jpg";
+    botonPedirCarta.disabled = false;
+    puntuacionActual.innerHTML = "0";
+    botonSimular.disabled = false;
+  });
+
+  botonSimular.addEventListener('click', () => {
+    let final = false;
+    do {
+      dameCarta();
+      let puntosSimulacion = parseFloat(puntuacionActual.innerHTML);
+      if (puntosSimulacion > 7.5) {
+        final = true;
+      }
+    } while (final == false);
+    alert('Ha terminado la simulacion.');
+    botonSimular.disabled = true;
+  });
 
 }
 
 
-
-
-// 1: https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/1_as-copas.jpg
-// 2: https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/2_dos-copas.jpg
-// 3: https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/3_tres-copas.jpg
-// 4: https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/4_cuatro-copas.jpg
-// 5: https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/5_cinco-copas.jpg
-// 6: https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/6_seis-copas.jpg
-// 7: https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/7_siete-copas.jpg
-// Sota: https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/10_sota-copas.jpg
-// Caballo: https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/11_caballo-copas.jpg
-// Rey: https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/12_rey-copas.jpg
-
-
+// EJERCICIO EN CLASE 
 
 // // GET DOM ELEMENTS:
 // const objetoNumeroUsuario = document.querySelector('#userNumber') as HTMLInputElement | null;
